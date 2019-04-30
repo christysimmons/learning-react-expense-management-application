116abecb72fcb8bac8bf30e410abb0212313ff15
Christina M Simmons <cmorrisond@gmail.com>
2019-04-30 12:20:30 -0700

    set up test database environment, split environments for test and production

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyCeM93-ybZ-cIkOSzq_gxwaYSHk0EWktlU",
-    authDomain: "expension-7a763.firebaseapp.com",
-    databaseURL: "https://expension-7a763.firebaseio.com",
-    projectId: "expension-7a763",
-    storageBucket: "expension-7a763.appspot.com",
-    messagingSenderId: "971098684763"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 
